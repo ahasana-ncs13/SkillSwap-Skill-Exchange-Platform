cd1a662461ebbebd7cbbf20b713c55d89d5b0054
ahasana-ncs13 <ahasanancs0485@gmail.com>
2025-11-10 21:00:58 +0600

    Completed responsive

--- src/Component/WhatUserSay/WhatUserSay.jsx
+++ src/Component/WhatUserSay/WhatUserSay.jsx
@@ -7,11 +7,11 @@ const WhatUserSay = ({userData}) => {
     const data = use(userData)
     return (
         <div className='mt-10 w-11/12 mx-auto'>
-            <div className="w-170 mx-auto text-center">
+            <div className="max-w-170 mx-auto text-center">
             <h1 className='text-3xl font-bold'>What Our Users Say</h1>
             <p className='text-gray-500 py-5'>See how learners and skill providers are growing together through SkillSwap. Real stories from real people who shared, taught, and learned new skills within their community.</p>
             </div>
-            <div className=" flex justify-between my-10 gap-5">
+            <div className=" w-11/12 mx-auto grid lg:grid-cols-3 grid-cols-1 my-10 gap-50">
             {
                  data.map(singleData => <UserSay key={singleData.id} singleData={singleData}></UserSay> )
             }
